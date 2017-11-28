56f0c17fbc35bfb274c628194971dc88a276755f
Bylevskiy <bylevskiy@restream.rt.ru>
2017-11-28 16:51:02 +0300

    Added rest check

--- checker.go
+++ checker.go
@@ -11,16 +11,18 @@ import (
 	"strconv"
 	"strings"
 	"time"
+	"encoding/json"
 )
 
 var Ss = map[string]int{}
 
 type CheckInstance struct {
-	CheckName string `yaml:"checkName"`
-	CheckEndpoint string `yaml:"checkEndpoint"`
-	CheckRateSeconds int `yaml:"checkRateSeconds"`
-	CheckSuccessString string `yaml:"checkSuccessString"`
-	CheckType string `yaml:"checkType"`
+	CheckName string `yaml:"Name"`
+	CheckEndpoint string `yaml:"Endpoint"`
+	CheckRateSeconds int `yaml:"RateSeconds"`
+	CheckSuccessString string `yaml:"SuccessString"`
+	CheckType string `yaml:"Type"`
+	CheckTimeout int `yaml:"Timeout"`
 }
 
 type Configs struct {
@@ -49,7 +51,20 @@ func main() {
 func checkSheduler(config Configs) {
 	for i := 0; i < len(config.Cfgs); i++ {
 		if (strings.Contains("http", config.Cfgs[i].CheckType)) {
-			go checkHttpService(config.Cfgs[i].CheckEndpoint, config.Cfgs[i].CheckSuccessString, config.Cfgs[i].CheckRateSeconds,config.Cfgs[i].CheckName)
+			go checkHttpService(
+				config.Cfgs[i].CheckEndpoint,
+				config.Cfgs[i].CheckSuccessString,
+				config.Cfgs[i].CheckRateSeconds,
+				config.Cfgs[i].CheckName,
+				config.Cfgs[i].CheckTimeout)
+		}
+		if (strings.Contains("rest", config.Cfgs[i].CheckType)) {
+			go checkJsonService(
+				config.Cfgs[i].CheckEndpoint,
+				config.Cfgs[i].CheckSuccessString,
+				config.Cfgs[i].CheckRateSeconds,
+				config.Cfgs[i].CheckName,
+				config.Cfgs[i].CheckTimeout)
 		}
 	}
 	go webMetricsService()
@@ -58,15 +73,74 @@ func checkSheduler(config Configs) {
 	}
 }
 
-func checkHttpService(checkEndpoint string, checkSuccessString string, checkRateSeconds int, checkName string) {
+type JsonState struct {
+	JsonServiceState string `json:"status"`
+}
+
+func checkJsonService(
+	checkEndpoint string,
+	checkSuccessString string,
+	checkRateSeconds int,
+	checkName string,
+	checkTimeout int) {
+	tr := &http.Transport{
+		IdleConnTimeout: 1000 * time.Millisecond * time.Duration(checkTimeout),
+	}
+	client := &http.Client{Transport:tr}
+	var returnState int
+	for {
+		time.Sleep(1000 * time.Millisecond * time.Duration(checkRateSeconds))
+		resp, err := client.Get(checkEndpoint)
+		if (err == nil) && (resp.StatusCode == 200) {
+			if (err != nil){
+				fmt.Println(err)
+			}
+			body, err := ioutil.ReadAll(resp.Body)
+			if (err != nil){
+				fmt.Println("Unable to read json answer.", err)
+			}
+			s, err := getJsonState([]byte(body))
+			if (err == nil) && (strings.Contains(s.JsonServiceState, checkSuccessString)) {
+				returnState = 1
+				Ss[checkName + "_rest_state_up"] = returnState
+			} else {
+				returnState = 0
+				Ss[checkName + "_rest_state_up"] = returnState
+			}
+		} else {
+			returnState = 0
+			Ss[checkName + "_rest_state_up"] = returnState
+		}
+	}
+}
+
+func getJsonState (body []byte) (*JsonState, error) {
+	var jsonState = new(JsonState)
+	err := json.Unmarshal(body, &jsonState)
+	if (err != nil){
+		fmt.Println("Unable to read json answer.", err)
+	}
+	return jsonState, err
+}
+
+func checkHttpService(
+	checkEndpoint string,
+	checkSuccessString string,
+	checkRateSeconds int,
+	checkName string,
+	checkTimeout int) {
 	var returnState int
 	checkSuccessInt, err := strconv.Atoi(checkSuccessString)
 	if (err != nil) {
 		fmt.Println("Unable to convert string to int success state for endpoint: " + checkEndpoint)
 	}
+	tr := &http.Transport{
+		IdleConnTimeout: 1000 * time.Millisecond * time.Duration(checkTimeout),
+		}
+	client := &http.Client{Transport:tr}
 	for {
 		time.Sleep(1000 * time.Millisecond * time.Duration(checkRateSeconds))
-		resp, err := http.Get(checkEndpoint)
+		resp, err := client.Get(checkEndpoint)
 		if (err == nil) && (resp.StatusCode == checkSuccessInt) {
 			returnState = 1
 			Ss[checkName + "_http_state_up"] = returnState
