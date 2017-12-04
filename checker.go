67baebf4eac94c38b048c9e39208c62354ddb0c0
Bylevskiy <bylevskiy@restream.rt.ru>
2017-12-04 12:51:02 +0300

    Added logging.

--- checker.go
+++ checker.go
@@ -12,9 +12,17 @@ import (
 	"strings"
 	"time"
 	"encoding/json"
+	"io"
 )
 
 var Ss = map[string]int{}
+
+var (
+	Trace   *log.Logger
+	Info    *log.Logger
+	Warning *log.Logger
+	Error   *log.Logger
+)
 
 type CheckInstance struct {
 	CheckName string `yaml:"Name"`
@@ -30,12 +38,13 @@ type Configs struct {
 }
 
 func main() {
+	LogInit(ioutil.Discard, os.Stdout, os.Stdout, os.Stderr)
 	//Get config
 	confFilePath, _ := filepath.Abs(os.Getenv("CHECKER_CONF_FILE"))
 	yamlFile, err := ioutil.ReadFile(confFilePath)
 
 	if err != nil {
-		fmt.Println("Config file not found " + confFilePath)
+		Error.Println("Config file not found " + confFilePath)
 		panic(err)
 	}
 	//Parse yaml
@@ -43,7 +52,7 @@ func main() {
 
 	err = yaml.Unmarshal(yamlFile, &config)
 	if err != nil {
-		log.Fatalf("error: %v", err)
+		Error.Println("error: %v", err)
 	}
 	checkSheduler(config)
 }
@@ -57,6 +66,7 @@ func checkSheduler(config Configs) {
 				config.Cfgs[i].CheckRateSeconds,
 				config.Cfgs[i].CheckName,
 				config.Cfgs[i].CheckTimeout)
+			Info.Println(config.Cfgs[i].CheckName + " service was added.")
 		}
 		if (strings.Contains("rest", config.Cfgs[i].CheckType)) {
 			go checkJsonService(
@@ -65,12 +75,37 @@ func checkSheduler(config Configs) {
 				config.Cfgs[i].CheckRateSeconds,
 				config.Cfgs[i].CheckName,
 				config.Cfgs[i].CheckTimeout)
+			Info.Println(config.Cfgs[i].CheckName + " service was added.")
 		}
 	}
 	go webMetricsService()
 	for {
 		time.Sleep(1000 * time.Millisecond * time.Duration(10))
 	}
+}
+
+
+func LogInit(
+	traceHandle io.Writer,
+	infoHandle io.Writer,
+	warningHandle io.Writer,
+	errorHandle io.Writer) {
+
+	Trace = log.New(traceHandle,
+		"TRACE: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+
+	Info = log.New(infoHandle,
+		"INFO: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+
+	Warning = log.New(warningHandle,
+		"WARNING: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+
+	Error = log.New(errorHandle,
+		"ERROR: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
 }
 
 type JsonState struct {
@@ -100,6 +135,7 @@ func checkJsonService(
 				fmt.Println("Unable to read json answer.", err)
 				returnState = 0
 				Ss[checkName + "_rest_state_up"] = returnState
+				Warning.Println(checkName + " service is not available now.")
 			} else {
 				s, err := getJsonState([]byte(body))
 				if (err == nil) && (strings.Contains(s.JsonServiceState, checkSuccessString)) {
@@ -108,11 +144,13 @@ func checkJsonService(
 				} else {
 					returnState = 0
 					Ss[checkName+"_rest_state_up"] = returnState
+					Warning.Println(checkName + " service is not available now.")
 				}
 			}
 		} else {
 			returnState = 0
 			Ss[checkName + "_rest_state_up"] = returnState
+			Warning.Println(checkName + " service is not available now.")
 		}
 	}
 }
@@ -150,11 +188,13 @@ func checkHttpService(
 		} else {
 			returnState = 0
 			Ss[checkName + "_http_state_up"] = returnState
+			Warning.Println(checkName + " service is not available now.")
 		}
 	}
 }
 
 func webMetricsService() {
+	Info.Println("Starting web-services...")
 	http.HandleFunc("/metrics", handler)
 	http.ListenAndServe(":8080", nil)
 }
